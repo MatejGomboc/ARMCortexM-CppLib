15368d0ca225140ca5d128668d89b0a7b1b75308
MatejGomboc-Claude-MCP <201144475+MatejGomboc-Claude-MCP@users.noreply.github.com>
2025-06-05 05:53:37 +0200

    Fix Cortex-M1 SCB to match M0 style with unions

--- cortexm1/scb.hpp
+++ cortexm1/scb.hpp
@@ -16,33 +16,173 @@
 
 #pragma once
 
-#include "utils.hpp"
 #include <cstdint>
 
 namespace CortexM1::Scb {
     static uint32_t* const BASE_ADDR = reinterpret_cast<uint32_t*>(0xE000ED00);
 
-    enum class SleepMode : uint8_t {
-        NORMAL = 0,
-        DEEP = 1
+    //! is a read-only register and contains the processor part number, version, and implementation information
+    union Cpuid {
+        struct Bits {
+            uint32_t revision: 4; //!< the p value in the Rnpn product revision identifier, indicates patch release
+            uint32_t part_no: 12; //!< part number of the processor (0xC61: Cortex-M1)
+            uint32_t constant: 4; //!< constant that defines the architecture of the processor (0xC: ARMv6-M architecture)
+            uint32_t variant: 4; //!< variant number: the r value in the Rnpn product revision identifier
+            uint32_t implementer: 8; //!< implementer code (0x41: ARM)
+        } bits;
+
+        uint32_t value = 0;
+
+        Cpuid() = default;
+
+        Cpuid(uint32_t new_value)
+        {
+            value = new_value;
+        }
     };
 
-    enum class VectorTableBase : uint8_t {
-        CODE = 0,
-        SRAM = 1
+    //! interrupt control and state register
+    union Icsr {
+        struct Bits {
+            uint32_t active_exception: 9; //!< exception number of the currently active exception
+            uint32_t reserved0: 3;
+            uint32_t pending_exception: 9; //!< exception number of the highest priority pending enabled exception
+            uint32_t reserved1: 1;
+            uint32_t irq_is_pending: 1; //!< true if an external configurable, NVIC generated, interrupt is pending
+            uint32_t exception_is_pending: 1; //!< true if a pending exception is serviced on exit from debug halt state
+            uint32_t reserved2: 1;
+            uint32_t pending_sys_tick_clear: 1; //!< removes the pending state from the SysTick exception
+            uint32_t pending_sys_tick_set: 1; //!< changes SysTick exception state to pending
+            uint32_t pending_sv_clear: 1; //!< removes the pending state from the PendSV exception
+            uint32_t pending_sv_set: 1; //!< change PendSV exception state to pending
+            uint32_t reserved3: 2;
+            uint32_t nmi_pending_set: 1; //!< changes NMI exception state to pending
+        } bits;
+
+        uint32_t value = 0;
+
+        Icsr() = default;
+
+        Icsr(uint32_t new_value)
+        {
+            value = new_value;
+        }
+    };
+
+    //! enables system reset
+    union Aircr {
+        static constexpr uint16_t VECT_KEY = 0x05FA; //!< magic number used for enabling writing to Aircr
+
+        struct Bits {
+            uint32_t reserved0: 1;
+            uint32_t vect_clr_active: 1; //!< reserved, write '0' to this bit
+            uint32_t sys_reset_req: 1; //!< requests a system reset
+            uint32_t reserved1: 12;
+            uint32_t endianness: 1; //!< reads as 0 - little endian
+            uint32_t vect_key: 16; //!< on writes, VECT_KEY to this field, otherwise the write is ignored
+        } bits;
+
+        uint32_t value = 0;
+
+        Aircr() = default;
+
+        Aircr(uint32_t new_value)
+        {
+            value = new_value;
+        }
+    };
+
+    //! controls features of entry to and exit from low power state
+    union Scr {
+        struct Bits {
+            uint32_t reserved0: 1;
+            uint32_t sleep_on_isr_exit: 1; //!< enter sleep or deep sleep on return from ISR
+            uint32_t use_deep_sleep: 1; //!< use deep sleep as low power mode
+            uint32_t reserved1: 1;
+
+            //! 0: only enabled exceptions or events can wakeup the processor, disabled exceptions are excluded
+            //! 1: enabled events and all exceptions, including disabled exceptions, can wakeup the processor
+            uint32_t send_event_on_except_pending: 1;
+
+            uint32_t reserved2: 27;
+        } bits;
+
+        uint32_t value = 0;
+
+        Scr() = default;
+
+        Scr(uint32_t new_value)
+        {
+            value = new_value;
+        }
+    };
+
+    //! is a read-only register and indicates some aspects of the behaviour of the processor
+    union Ccr {
+        struct Bits {
+            uint32_t reserved0: 3;
+            uint32_t fault_on_unaligned_access: 1; //!< always '1', indicates that all unaligned accesses generate a HardFault
+            uint32_t reserved1: 4;
+            uint32_t stack_alignment: 1; //!< always '1', indicates 8-byte stack alignment on exception entry
+            uint32_t reserved2: 23;
+        } bits;
+
+        uint32_t value = 0;
+
+        Ccr() = default;
+
+        Ccr(uint32_t new_value)
+        {
+            value = new_value;
+        }
+    };
+
+    //! sets the priority level of the exception handlers that have configurable priority (SVCall)
+    union Shpr2 {
+        struct Bits {
+            uint32_t reserved0: 24;
+            uint32_t sv_call_except_priority: 8; //!< priority of SVCall exception
+        } bits;
+
+        uint32_t value = 0;
+
+        Shpr2() = default;
+
+        Shpr2(uint32_t new_value)
+        {
+            value = new_value;
+        }
+    };
+
+    //! sets the priority level of the exception handlers that have configurable priority (PendSV, SysTick)
+    union Shpr3 {
+        struct Bits {
+            uint32_t reserved0: 16;
+            uint32_t pend_sv_except_priority: 8; //!< priority of PendSV exception
+            uint32_t sys_tick_except_priority: 8; //!< priority of SysTick exception
+        } bits;
+
+        uint32_t value = 0;
+
+        Shpr3() = default;
+
+        Shpr3(uint32_t new_value)
+        {
+            value = new_value;
+        }
     };
 
     struct Registers
     {
-        volatile const uint32_t cpuid; //!< Contains the processor part number, version, and implementation information
-        volatile uint32_t icsr; //!< Provides system exception numbers and states
-        volatile uint32_t reserved0;
-        volatile uint32_t aircr; //!< Application interrupt and reset control
-        volatile uint32_t scr; //!< System control
-        volatile uint32_t ccr; //!< Configuration and control
+        volatile uint32_t cpuid; //!< contains the processor part number, version, and implementation information
+        volatile uint32_t icsr; //!< interrupt control and state register
+        volatile uint32_t vtor; //!< vector table offset register
+        volatile uint32_t aircr; //!< enables system reset
+        volatile uint32_t scr; //!< controls features of entry to and exit from low power state
+        volatile uint32_t ccr; //!< is a read-only register and indicates some aspects of the behaviour of the processor
         volatile uint32_t reserved1;
-        volatile uint32_t shpr2; //!< System handler priority register 2
-        volatile uint32_t shpr3; //!< System handler priority register 3
+        volatile uint32_t shpr2; //!< sets the priority level of the exception handlers that have configurable priority (SVCall)
+        volatile uint32_t shpr3; //!< sets the priority level of the exception handlers that have configurable priority (PendSV, SysTick)
     };
 
     static inline volatile Registers* registers()
@@ -50,152 +190,19 @@ namespace CortexM1::Scb {
         return reinterpret_cast<volatile Registers*>(BASE_ADDR);
     }
 
-    // CPUID register functions
-    static inline uint32_t getCpuId()
-    {
-        return registers()->cpuid;
-    }
-
-    // ICSR register functions
-    static inline bool isVectorActive()
-    {
-        return (registers()->icsr & 0x1FF) != 0;
-    }
-
-    static inline uint8_t getActiveVectorNumber()
-    {
-        return registers()->icsr & 0x1FF;
-    }
-
-    static inline bool isVectorPending()
-    {
-        return Utils::isBitSet(registers()->icsr, 22);
-    }
-
-    static inline uint8_t getPendingVectorNumber()
-    {
-        return (registers()->icsr >> 12) & 0x1FF;
-    }
-
-    static inline void setPendStClr()
-    {
-        Utils::setBit(registers()->icsr, 25);
-    }
-
-    static inline void setPendStSet()
-    {
-        Utils::setBit(registers()->icsr, 26);
-    }
-
-    static inline void setPendSvClr()
-    {
-        Utils::setBit(registers()->icsr, 27);
-    }
-
-    static inline void setPendSvSet()
-    {
-        Utils::setBit(registers()->icsr, 28);
-    }
-
-    static inline bool isNmiPending()
-    {
-        return Utils::isBitSet(registers()->icsr, 31);
-    }
-
-    static inline void setNmiPending()
-    {
-        Utils::setBit(registers()->icsr, 31);
-    }
-
-    // AIRCR register functions
-    static inline void requestSystemReset()
+    __attribute__((noreturn)) static inline void systemReset()
     {
         asm volatile("DSB" : : : "memory");
-        registers()->aircr = 0x05FA0004;
+
+        Aircr aircr { registers()->aircr };
+
+        aircr.bits.sys_reset_req = true;
+        aircr.bits.vect_key = Aircr::VECT_KEY;
+
+        registers()->aircr = aircr.value;
+
         asm volatile("DSB" : : : "memory");
-        while (true) { asm volatile("NOP"); }
-    }
 
-    static inline bool isLittleEndian()
-    {
-        return !Utils::isBitSet(registers()->aircr, 15);
-    }
-
-    // SCR register functions
-    static inline void setSleepMode(SleepMode mode)
-    {
-        if (mode == SleepMode::DEEP) {
-            Utils::setBit(registers()->scr, 2);
-        } else {
-            Utils::clearBit(registers()->scr, 2);
-        }
-    }
-
-    static inline void setSleepOnExit(bool enable)
-    {
-        if (enable) {
-            Utils::setBit(registers()->scr, 1);
-        } else {
-            Utils::clearBit(registers()->scr, 1);
-        }
-    }
-
-    static inline void setEventOnPending(bool enable)
-    {
-        if (enable) {
-            Utils::setBit(registers()->scr, 4);
-        } else {
-            Utils::clearBit(registers()->scr, 4);
-        }
-    }
-
-    // CCR register functions
-    static inline void setUnalignedAccessTrap(bool enable)
-    {
-        if (enable) {
-            Utils::setBit(registers()->ccr, 3);
-        } else {
-            Utils::clearBit(registers()->ccr, 3);
-        }
-    }
-
-    static inline void setStackAlign8(bool enable)
-    {
-        if (enable) {
-            Utils::setBit(registers()->ccr, 9);
-        } else {
-            Utils::clearBit(registers()->ccr, 9);
-        }
-    }
-
-    // SHPR register functions
-    static inline void setSvCallPriority(uint8_t priority)
-    {
-        registers()->shpr2 = (registers()->shpr2 & 0x00FFFFFF) | (static_cast<uint32_t>(priority) << 24);
-    }
-
-    static inline uint8_t getSvCallPriority()
-    {
-        return (registers()->shpr2 >> 24) & 0xFF;
-    }
-
-    static inline void setPendSvPriority(uint8_t priority)
-    {
-        registers()->shpr3 = (registers()->shpr3 & 0xFFFF00FF) | (static_cast<uint32_t>(priority) << 16);
-    }
-
-    static inline uint8_t getPendSvPriority()
-    {
-        return (registers()->shpr3 >> 16) & 0xFF;
-    }
-
-    static inline void setSysTickPriority(uint8_t priority)
-    {
-        registers()->shpr3 = (registers()->shpr3 & 0x00FFFFFF) | (static_cast<uint32_t>(priority) << 24);
-    }
-
-    static inline uint8_t getSysTickPriority()
-    {
-        return (registers()->shpr3 >> 24) & 0xFF;
+        while(true);
     }
 }
