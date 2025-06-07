7ad17249ee219fb8a879f6a98c77a4304d84c92d
Matej Gomobc <matejg03@gmail.com>
2025-06-07 19:28:37 +0200

    Harmonised with M0+.

--- cortexm0/systick.hpp
+++ cortexm0/systick.hpp
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2025 Matej Gomboc <https://github.com/MatejGomboc/ARMCortexM-CppLib>
 
-    Licensed under the Apache License, Version 2.0 (the "Licence");
+    Licensed under the Apache Licence, Version 2.0 (the "Licence");
     you may not use this file except in compliance with the Licence.
     You may obtain a copy of the Licence at
 
@@ -21,28 +21,49 @@
 namespace CortexM0::SysTick {
     static uint32_t* const BASE_ADDR = reinterpret_cast<uint32_t*>(0xE000E010);
 
-    //! SysTick control and status register
-    union CtrlStat {
-        //! SysTick clock source
-        enum class ClockSource : bool {
+    union CtrlStatus {
+        //! timer clock source
+        enum class ClkSource : bool {
             EXTERNAL = false, //!< external reference clock
-            PROCESSOR = true //!< processor clock
+            CPU = true //!< processor clock
         };
 
         struct Bits {
-            uint32_t enabled: 1; //!< enable the SysTick
-            uint32_t irq_enabled: 1; //!< enable the SysTick interrupt
-            uint32_t clock_source: 1; //!< SysTick clock source
-            uint32_t reserved: 13;
-            uint32_t count_flag: 1; //!< returns 1 if timer counted to 0 since the last time this was read
-            uint32_t reserved2: 15;
+            uint32_t timer_enabled: 1; //!< enables counting
+            uint32_t exception_enabled: 1; //!< enables SysTick exception
+            uint32_t clk_source: 1; //!< selects the timer clock source
+            uint32_t reserved0: 13;
+            uint32_t reached_zero: 1; //!< '1' if counter reached 0 since last time this bit was read
+            uint32_t reserved1: 15;
         } bits;
 
         uint32_t value = 0;
 
-        CtrlStat() = default;
+        CtrlStatus() = default;
 
-        CtrlStat(uint32_t new_value)
+        CtrlStatus(uint32_t new_value)
+        {
+            value = new_value;
+        }
+    };
+
+    union Calibration {
+        struct Bits {
+            /*!
+                Indicates the calibration value when the SysTick counter runs on HCLK max/8 as external clock.
+                When HCLK is programmed at the maximum frequency, the SysTick period is 1ms.
+            */
+            uint32_t calib_val: 24;
+            uint32_t reserved: 6;
+            uint32_t skew: 1; //!< always '1'
+            uint32_t noref: 1; //!< always '0', indicates that a separate reference clock is provided (HCLK/8)
+        } bits;
+
+        uint32_t value = 0;
+
+        Calibration() = default;
+
+        Calibration(uint32_t new_value)
         {
             value = new_value;
         }
@@ -50,39 +71,14 @@ namespace CortexM0::SysTick {
 
     struct Registers
     {
-        volatile uint32_t ctrl_stat; //!< control and status register
-        volatile uint32_t reload_val; //!< reload value register
-        volatile uint32_t current_val; //!< current value register
-        volatile uint32_t calib_val; //!< calibration value register
+        volatile uint32_t ctrl_status; //!< control and status register
+        volatile uint32_t reload_val; //!< reload value at the restart of counting
+        volatile uint32_t current_val; //!< current counter value
+        volatile uint32_t calibration; //!< controls timer calibration
     };
 
     static inline volatile Registers* registers()
     {
         return reinterpret_cast<volatile Registers*>(BASE_ADDR);
-    }
-
-    static inline void setReloadValue(uint32_t value)
-    {
-        registers()->reload_val = value & 0x00FFFFFF;
-    }
-
-    static inline uint32_t getReloadValue()
-    {
-        return registers()->reload_val & 0x00FFFFFF;
-    }
-
-    static inline void setCurrentValue(uint32_t value)
-    {
-        registers()->current_val = value & 0x00FFFFFF;
-    }
-
-    static inline uint32_t getCurrentValue()
-    {
-        return registers()->current_val & 0x00FFFFFF;
-    }
-
-    static inline uint32_t getCalibrationValue()
-    {
-        return registers()->calib_val;
     }
 }
