18331f53a1eb6464a5a46e10d821f834ff696c6d
Matej Gomobc <matejg03@gmail.com>
2025-07-17 16:11:11 +0200

    Updated tests.

--- asm_tests/example.cpp
+++ asm_tests/example.cpp
@@ -1,7 +1,7 @@
 // RUN: arm-none-eabi-g++ -mcpu=cortex-m0plus -mthumb -std=c++17 -S %s -o %t.asm
 // RUN: FileCheck %s < %t.asm
 
-[[naked]] void example() {
+__attribute__((naked)) void example() {
     // CHECK: nop
     asm volatile("nop");
 }
