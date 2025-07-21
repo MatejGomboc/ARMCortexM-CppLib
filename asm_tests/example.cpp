cd7167ba02883a3fa43de4d7466733c42dbe7249
Matej Gomobc <matejg03@gmail.com>
2025-07-21 11:14:52 +0200

    Typo fix.

--- asm_tests/example.cpp
+++ asm_tests/example.cpp
@@ -1,4 +1,4 @@
-// RUN: arm-none-eabi-g++ %arch_flag% -mthumb -mfloat-abi=soft -masm-syntax-unified -std=gnu++17 %default_flags% -S %s -o %t.asm
+// RUN: arm-none-eabi-g++ %arch_flag% -mthumb -mfloat-abi=soft -masm-syntax-unified -std=c++17 %default_flags% -S %s -o %t.asm
 // RUN: FileCheck %s < %t.asm
 
 void example() {
