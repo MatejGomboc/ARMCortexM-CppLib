e622d7cd47840c63e81e542aa1ef5c8a0f73fb96
Matej Gomobc <matejg03@gmail.com>
2025-07-21 15:30:11 +0200

    Using CMake's native CTest framework instead of LIT.

--- asm_tests/example.cpp
+++ asm_tests/example.cpp
@@ -1,7 +1,3 @@
-// RUN: arm-none-eabi-g++ %arch_flag% -mthumb -mfloat-abi=soft -masm-syntax-unified -std=c++17 %default_flags% -c %s -o %t.o
-// RUN: arm-none-eabi-objdump -d --no-show-raw-insn %t.o > %t.asm
-// RUN: FileCheck %s --input-file %t.asm
-
 extern "C" void example() {
     asm volatile("nop");
 }
