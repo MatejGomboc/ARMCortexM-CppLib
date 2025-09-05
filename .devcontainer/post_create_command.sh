8a8d1b2737cd7972bacd2198e5af02fbd2ee12f6
Matej Gomobc <matejg03@gmail.com>
2025-09-05 21:55:40 +0200

    Fixes in .devcontainer

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
+# Update vcpkg registries
 vcpkg x-update-registry --all
+
+# Install dependencies from vcpkg-configuration.json
+vcpkg install --triplet arm-none-eabi
+
+# Activate the environment
 vcpkg activate
