655be2b4484bdaa6497c0e4edb196af855944c47
Matej Gomobc <matejg03@gmail.com>
2025-09-06 11:59:56 +0200

    Fixes in .devcontainer

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+export Z_VCPKG_POSTSCRIPT="$(mktemp).sh"
+
 vcpkg x-update-registry --all
 vcpkg activate
+
+if [ -f "${Z_VCPKG_POSTSCRIPT}" ]; then
+    . "${Z_VCPKG_POSTSCRIPT}"
+    rm "${Z_VCPKG_POSTSCRIPT}"
+fi
