759169e2ebee472ba892c628d245d83b8976c7c2
Matej Gomobc <matejg03@gmail.com>
2025-10-28 18:52:25 +0000

    Added a stub for bit utils tests.

---
+++ tests/bit_utils/test_is_bit_set.cpp
@@ -0,0 +1,33 @@
+#include "bit_utils.hpp"
+
+extern "C" [[gnu::naked]] bool test_is_bit_set_static_8_0_false() {
+    return Cortex::isBitSet(uint8_t{0x00}, uint8_t{0});
+}
+
+// CHECK-LABEL: <test_is_bit_set_static_8_0_false>:
+// CHECK-NEXT: movs r0, #0
+// CHECK-EMPTY:
+
+extern "C" [[gnu::naked]] bool test_is_bit_set_static_8_0_true() {
+    return Cortex::isBitSet(uint8_t{0x01}, uint8_t{0});
+}
+
+// CHECK-LABEL: <test_is_bit_set_static_8_0_true>:
+// CHECK-NEXT: movs r0, #1
+// CHECK-EMPTY:
+
+extern "C" [[gnu::naked]] bool test_is_bit_set_static_8_4_false() {
+    return Cortex::isBitSet(uint8_t{0x00}, uint8_t{3});
+}
+
+// CHECK-LABEL: <test_is_bit_set_static_8_4_false>:
+// CHECK-NEXT: movs r0, #0
+// CHECK-EMPTY:
+
+extern "C" [[gnu::naked]] bool test_is_bit_set_static_8_4_true() {
+    return Cortex::isBitSet(uint8_t{0x08}, uint8_t{3});
+}
+
+// CHECK-LABEL: <test_is_bit_set_static_8_4_true>:
+// CHECK-NEXT: movs r0, #1
+// CHECK-EMPTY:
