69e16dec69d9184b75449db1444108b9dcd06386
Matej Gomobc <matejg03@gmail.com>
2025-11-09 15:48:59 +0000

    Updated Utils tests.

--- tests/bit_utils/test_is_bit_set_runtime.cpp
+++ tests/bit_utils/test_is_bit_set_runtime.cpp
@@ -9,6 +9,31 @@ extern "C" [[gnu::naked]] bool test_is_bit_set_runtime_8_0(uint8_t value) {
 }
 
 // CHECK-LABEL: <test_is_bit_set_runtime_8_0>:
-// CHECK-NEXT: movs r0, #0
-// MAXSPEED-CHECK-NEXT: nop
+
+// DEBUG-CHECK-NEXT: lsls r0, r0, #31
+// DEBUG-CHECK-NEXT: lsrs r0, r0, #31
+
+// MINSIZE-CHECK-NEXT: movs r3, #1
+// MINSIZE-CHECK-NEXT: ands r0, r3
+
+// MAXSPEED-CHECK-NEXT: movs r3, #1
+// MAXSPEED-CHECK-NEXT: ands r0, r3
+
+// CHECK-EMPTY:
+
+extern "C" [[gnu::naked]] bool test_is_bit_set_runtime_8_3(uint8_t value) {
+    return Cortex::isBitSet(value, uint8_t{3});
+}
+
+// CHECK-LABEL: <test_is_bit_set_runtime_8_3>:
+// CHECK-NEXT: lsls r0, r0, #28
+// CHECK-NEXT: lsrs r0, r0, #31
+// CHECK-EMPTY:
+
+extern "C" [[gnu::naked]] bool test_is_bit_set_runtime_8_7(uint8_t value) {
+    return Cortex::isBitSet(value, uint8_t{7});
+}
+
+// CHECK-LABEL: <test_is_bit_set_runtime_8_7>:
+// CHECK-NEXT: lsrs r0, r0, #7
 // CHECK-EMPTY:
